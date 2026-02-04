325ee77d7fd0fbc309df392de84974538151ee61
Thura <thura.00011@gmail.com>
2026-02-04 17:35:26 +0630

    hotfix: update base URL in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/weekly-planner-pwa-app/',
+  base: '/weekly-planner/',
   plugins: [
     react(),
     tailwindcss(),
